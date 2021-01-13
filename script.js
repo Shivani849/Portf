7006025ee4d300aa57c4beb7b57e9d1e298765d9
Shivani849 <shivanim849@gmail.com>
2021-01-13 15:38:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+        
+});
+
